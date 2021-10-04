37c9c88ce49a25b1bb2ce3f04da473b33511cac4
Ankit Vishwakarma <ankvis5826@gmail.com>
2021-10-05 01:50:18 +0530

    make your Color js page done v.1 of MYC

---
+++ test/firstFinal/makeShade.js
@@ -0,0 +1,126 @@
+
+export class makeColorShade {
+    constructor() {
+        
+    }
+    makeDrak(r, g, b) {
+        //for making Drack doing minus 
+        let drakShadeArray = [];
+        let R = r;
+        let G = g;
+        let B = b;
+        const minus = 15;
+        for (let i = 0; i < 8; i++) {
+
+            if (R <=0 & G <= 0 & B <= 0) {
+                return drakShadeArray;
+            }
+            //this is for the r color
+            if (R <= 15) {
+                R = 0;
+            } else {
+                R = R - minus;
+            }
+            //this is for the g color
+
+            if (G <= 15) {
+                G = 0;
+            } else {
+                G = G - minus;
+            }
+            //this is for the b color
+            if (B <= 15) {
+                B = 0;  
+            } else {
+                B = B - minus;
+            }
+            drakShadeArray[i] = { "r": R, "g": G, "b": B };
+        }
+        return drakShadeArray;
+    }
+    //this function is for light shade
+    makelight(r, g, b) {
+        //for making Drack doing minus 
+        let lightShadeArray = [];
+        let R = Number(r);
+        let G = Number(g);
+        let B = Number(b);
+        console.log(R+" "+G+" "+B);
+        const pluse = 15;
+        for (let i = 0; i < 8; i++) {
+            if ((R >=240) & (G >= 240) & (B >= 240)) {
+                console.log('first return');
+                console.log(R+" "+G+" "+B);
+
+                return lightShadeArray;
+                
+            }
+            //this is for the r color
+            if (R >= 255) {
+                R = 255;
+               
+
+            } else {
+                R = R + pluse;
+
+
+            }
+            //this is for the g color
+
+            if (G  >= 255) {
+                G = 255;
+
+            
+            } else {
+                G =G+ pluse;
+
+               
+            }
+            //this is for the b color
+            if (B  >= 255) {
+                B = 255;
+
+            } else {
+                B = B + pluse;
+                
+
+            }
+            lightShadeArray[i] = { "r": R, "g": G, "b": B };
+
+        }
+        console.log(lightShadeArray);
+        console.log(R+" "+G+" "+B)
+        return lightShadeArray;
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/firstFinal/makeYourColor.js
+++ test/firstFinal/makeYourColor.js
@@ -1,5 +1,5 @@
-
-import {randomColor } from './randamColor.js';
+import { makeColorShade } from './makeShade.js';
+import { randomColor } from './randamColor.js';
 //"MYC" stand for makeYourColor page 
 
 //for geting all use able Html element/
@@ -12,74 +12,192 @@ let MYCcontrolDiv = document.querySelectorAll("#ColorCodeMYC .inputControl");
 
 
 let MYCcontrolBar = {
-    R:MYCcontrolDiv[0].querySelector(".MYCrangeBar"),
-    G:MYCcontrolDiv[1].querySelector(".MYCrangeBar"),
-    B:MYCcontrolDiv[2].querySelector(".MYCrangeBar")
+    R: MYCcontrolDiv[0].querySelector(".MYCrangeBar"),
+    G: MYCcontrolDiv[1].querySelector(".MYCrangeBar"),
+    B: MYCcontrolDiv[2].querySelector(".MYCrangeBar")
 };
-  //---***for input Box('Number')
+//---***for input Box('Number')
 let MYCcontrolBarInput = {
-    Rinput:MYCcontrolDiv[0].querySelector(".MYCrangeShow"),
-    Ginput:MYCcontrolDiv[1].querySelector(".MYCrangeShow"),
-    Binput:MYCcontrolDiv[2].querySelector(".MYCrangeShow")
+    Rinput: MYCcontrolDiv[0].querySelector(".MYCrangeShow"),
+    Ginput: MYCcontrolDiv[1].querySelector(".MYCrangeShow"),
+    Binput: MYCcontrolDiv[2].querySelector(".MYCrangeShow")
 };
 //---this is for the code section
-let MYCrgbCode= document.querySelector("#ColorCodeMYC .output .rgbColorCodeMYC");
-let MYChexCode= document.querySelector("#ColorCodeMYC .output .hexColorCodeMYC");
-
+let MYCrgbCode = document.querySelector("#ColorCodeMYC .output .rgbColorCodeMYC");
+let MYChexCode = document.querySelector("#ColorCodeMYC .output .hexColorCodeMYC");
+//_______________this is for the second half___________________
+//for ide
+let MYCideWrite = document.querySelector('#ideMYC .four');
 
 //function started
 //---this for the all first color output which ramdomly come
 let random = new randomColor;
 let randomColorCode = random.value;
+//-----geting color value
+let R = randomColorCode[0];
+let G = randomColorCode[1];
+let B = randomColorCode[2];
 
 
 for (const keys in MYCcontrolBar) {
-    MYCcontrolBar[keys].addEventListener('change',()=>{
+    MYCcontrolBar[keys].addEventListener('change', () => {
         onUserChageBar();
     })
-  }
+}
 
-  for (const keys in  MYCcontrolBarInput) {
-    MYCcontrolBarInput[keys].addEventListener('change',()=>{
+
+
+for (const keys in MYCcontrolBarInput) {
+    MYCcontrolBarInput[keys].addEventListener('change', () => {
         onUserChageInput();
-        
-    })
-  }
 
- function onUserChageBar(){
+    })
+}
+
+function onUserChageBar() {
     let UserR = MYCcontrolBar['R'].value;
     let UserG = MYCcontrolBar['G'].value;
     let UserB = MYCcontrolBar['B'].value;
-    MYCupdateUI(UserR,UserG,UserB);
- }
+    MYCupdateUI(UserR, UserG, UserB);
+    getShade(UserR, UserG, UserB);
 
- function onUserChageInput(){
+}
+
+function onUserChageInput() {
     let UserR = MYCcontrolBarInput['Rinput'].value;
     let UserG = MYCcontrolBarInput['Ginput'].value;
     let UserB = MYCcontrolBarInput['Binput'].value;
-    MYCupdateUI(UserR,UserG,UserB);
- }
+    MYCupdateUI(UserR, UserG, UserB);
+    getShade(UserR, UserG, UserB);
 
-
-
-function MYCupdateUI(r=randomColorCode[0],g=randomColorCode[1],b=randomColorCode[2]){
-   MYCcolorBox.style.backgroundColor = `rgb(${r},${g},${b})`;
-
-   //this is for the bar
-   MYCcontrolBar['R'].value =r;
-   MYCcontrolBar['G'].value =g;
-   MYCcontrolBar['B'].value =b;
    
-   //this is for input Box
-   MYCcontrolBarInput['Rinput'].value = r;
-   MYCcontrolBarInput['Ginput'].value = g;
-   MYCcontrolBarInput['Binput'].value = b;
+}
 
-   //this is for the code 
-   MYCrgbCode.innerHTML=`rgb(${r},${g},${b})`;
+function MYCupdateUI(r = R, g = G, b = B) {
+    MYCcolorBox.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-   MYChexCode.innerHTML="#"+Number(r).toString(16)+""+Number(g).toString(16)+""+Number(b).toString(16);
+    //this is for the bar
+    MYCcontrolBar['R'].value = r;
+    MYCcontrolBar['G'].value = g;
+    MYCcontrolBar['B'].value = b;
+
+    //this is for input Box
+    MYCcontrolBarInput['Rinput'].value = r;
+    MYCcontrolBarInput['Ginput'].value = g;
+    MYCcontrolBarInput['Binput'].value = b;
+
+    //this is for the code 
+    MYCrgbCode.innerHTML = `rgb(${r},${g},${b})`;
+
+
+    let hexCode = "#";
+    let check = Number(r).toString(16);
+    if (check <= 0) {
+        hexCode += "00";
+    }
+    else {
+        hexCode += check;
+    }
+    check = Number(g).toString(16);
+    if (check <= 0) {
+        hexCode += "00";
+    }
+    else {
+        hexCode += check;
+    }
+    check = Number(b).toString(16);
+    if (check <= 0) {
+        hexCode += "00";
+    }
+    else {
+        hexCode += check;
+    }
+    MYChexCode.innerHTML = hexCode;
+    //this is for ide 
+    MYCideWrite.innerHTML = `color:rgb(${r},${g},${b});`;
+
+    //this function is for the color shade section update 
+    
 }
 MYCupdateUI();
+
+//this is for the ide btn
+let MYCideTopBtn = document.querySelectorAll('#ideMYC .two ul li');
+
+MYCideTopBtn[0].addEventListener('click', () => {
+    MYCideWrite.innerHTML = "color:" + MYCrgbCode.innerHTML + ";"
+});
+
+MYCideTopBtn[1].addEventListener('click', () => {
+    MYCideWrite.innerHTML = "background-color:" + MYCrgbCode.innerHTML + ";"
+});
+
+
+// //this is for color shade
+let shade = new makeColorShade();
+function getShade(r, g, b) {
+    let dark = shade.makeDrak(r, g, b);
+
+    let light = shade.makelight(r, g, b);
+   
+    let allShades = [];
+    for (let i = 0; i < light.length; i++) {
+        allShades[i] = light[light.length -1 - i ];
+
+    }
+
+    for (let i = 0; i < dark.length; i++) {
+        allShades[light.length + i] = dark[i];
+    }
+   
+    updateShadeUI(allShades);
+    
+}
+
+function updateShadeUI(shades) {
+    let shadeDiv = document.querySelector('#ColorShadeSecMYC .colorShadeBox');
+    shadeDiv.innerHTML =""; 
+    for (let i = 0; i < shades.length; i++) {
+        //geting color value 
+        let r = shades[i].r;
+        let g = shades[i].g;
+        let b = shades[i].b;
+        
+        //this is for the hex code
+        let hexCode = "#";
+        let check = Number(r).toString(16);
+        if (check <= 0) {
+            hexCode += "00";
+        }
+        else {
+            hexCode += check;
+        }
+        check = Number(g).toString(16);
+        if (check <= 0) {
+            hexCode += "00";
+        }
+        else {
+            hexCode += check;
+        }
+        check = Number(b).toString(16);
+        if (check <= 0) {
+            hexCode += "00";
+        }
+        else {
+            hexCode += check;
+        }
+      
+        let sigleShade = ` <div class="shade">
+        <div class="shadeColor colorShadeItem colorBox" style="background-color:rgb(${r}, ${g}, ${b});"></div>
+        <div class="rgbCode colorShadeItem codeBox">rgb(${r}, ${g}, ${b})</div>
+        <div class="hexCode colorShadeItem codeBox">${hexCode}</div>
+      </div>`
+
+        shadeDiv.innerHTML += sigleShade;
+    }
+
+}
+
+getShade(R,G,B);
 
 
